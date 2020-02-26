b23923642dbc4cd350abb9bad8674bb6cff5eec9
catcarbonell <hello@catcarbn.com>
2020-02-26 10:55:37 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+// API Endpoints
+// Require necessities (express, bodyParser, etc)
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 4200;
+
+app.use(bodyParser.json());
